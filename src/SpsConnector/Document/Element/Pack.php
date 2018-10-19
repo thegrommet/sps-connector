9e944ee3f02051cbd337d8547cae57c96426a840
Todd Mannherz <todd@thegrommet.com>
2018-10-19 10:44:03 -0400

    Pack elements now generate SSCCs.

--- src/SpsConnector/Document/Element/Pack.php
+++ src/SpsConnector/Document/Element/Pack.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 
 namespace SpsConnector\Document\Element;
 
+use Exception;
 use SimpleXMLElement;
 use SpsConnector\Document\Exception\ElementInvalid;
-use SpsConnector\Document\Exception\ElementNotSet;
 
 /**
  * Pack element
@@ -16,35 +16,82 @@ class Pack implements ExportsXmlInterface
     const TYPE_PALLET  = 'T';
 
     public $type;
-    public $shippingSerialId;
+    public $gs1CompanyPrefix;
+    public $serialReference;
+    public $extensionDigit;
+    public $carrierPackageId;
 
-    public function __construct(string $type = null, string $shippingSerialId = null)
-    {
+    public function __construct(
+        string $type,
+        string $gs1CompanyPrefix,
+        string $serialReference,
+        string $extensionDigit = '0',
+        string $carrierPackageId = null
+    ) {
         $this->type = $type;
-        $this->shippingSerialId = $shippingSerialId;
+        $this->gs1CompanyPrefix = $gs1CompanyPrefix;
+        $this->serialReference = $serialReference;
+        $this->extensionDigit = $extensionDigit;
+        $this->carrierPackageId = $carrierPackageId;
     }
 
     public function exportToXml(SimpleXMLElement $parent): SimpleXMLElement
     {
-        if (!$this->type || !$this->shippingSerialId) {
-            throw new ElementNotSet('Pack: Both PackLevelType and ShippingSerialID must be set.');
-        }
         if ($this->type != self::TYPE_PACKAGE && $this->type != self::TYPE_PALLET) {
             throw new ElementInvalid('Pack: Invalid PackLevelType.');
         }
+        if (!$this->gs1CompanyPrefix || !$this->serialReference) {
+            throw new ElementInvalid('Pack: GS1 Company Prefix and Serial Reference must be set for SSCC generation.');
+        }
+        try {
+            $sscc = self::generateSSCC($this->gs1CompanyPrefix, $this->serialReference, $this->extensionDigit);
+        } catch (Exception $e) {
+            throw new ElementInvalid('Pack: ' . $e->getMessage());
+        }
         $root = $parent->addChild('Pack');
         $root->addChild('PackLevelType', $this->type);
-        $root->addChild('ShippingSerialID', $this->formatShippingSerialId());
+        $root->addChild('ShippingSerialID', $sscc);
+        if ($this->carrierPackageId) {
+            $root->addChild('CarrierPackageID', $this->carrierPackageId);
+        }
         return $root;
     }
 
     /**
-     * Format the ID to 20 chars, left padded with zeros.
+     * Generate an SSCC code.
      *
+     * @link https://www.gs1us.org/tools/check-digit-calculator
+     * @param string $gs1CompanyPrefix
+     * @param string $serialReference
+     * @param string $extensionDigit
      * @return string
      */
-    public function formatShippingSerialId(): string
-    {
-        return str_pad($this->shippingSerialId, 20, '0', STR_PAD_LEFT);
+    public static function generateSSCC(
+        string $gs1CompanyPrefix,
+        string $serialReference,
+        string $extensionDigit = '0'
+    ): string {
+        $serialReference = str_pad($serialReference, 16 - strlen($gs1CompanyPrefix), '0', STR_PAD_LEFT);
+        $digits = [(int)$extensionDigit];
+        foreach (str_split($gs1CompanyPrefix) as $digit) {
+            $digits[] = (int)$digit;
+        }
+        foreach (str_split($serialReference) as $digit) {
+            $digits[] = (int)$digit;
+        }
+        if (count($digits) != 17) {
+            throw new Exception(
+                'Invalid GS1 Company Prefix or Serial Reference - combined, they must not exceed 16 characters.'
+            );
+        }
+        $check = 0;
+        for ($i = 0; $i < 17; $i++) {
+            $check += $digits[$i] * ($i & 1 ? 1 : 3);
+        }
+        $check = 10 - $check % 10;
+        if ($check == 10) {
+            $check = 0;
+        }
+        return '00' . implode('', $digits) . (string)$check;
     }
 }
--- tests/Document/Element/PackTest.php
+++ tests/Document/Element/PackTest.php
@@ -7,7 +7,6 @@ use PHPUnit\Framework\TestCase;
 use SimpleXMLElement;
 use SpsConnector\Document\Element\Pack;
 use SpsConnector\Document\Exception\ElementInvalid;
-use SpsConnector\Document\Exception\ElementNotSet;
 
 /**
  * Pack Element Test Suite
@@ -16,28 +15,41 @@ class PackTest extends TestCase
 {
     public function testExportToXml(): void
     {
-        $pack = new Pack('P', '7000012345');
+        $pack = new Pack('P', '00000001', '00000001', '0', '7123456');
         $xml = new SimpleXMLElement('<root/>');
         $pack->exportToXml($xml);
         $this->assertSame('P', (string)$xml->Pack->PackLevelType);
-        $this->assertSame('00000000007000012345', (string)$xml->Pack->ShippingSerialID);
-    }
-
-    public function testExportToXmlRequired(): void
-    {
-        $pack = new Pack();
-        $xml = new SimpleXMLElement('<root/>');
-        $this->expectException(ElementNotSet::class);
-        $this->expectExceptionMessage('Pack: Both PackLevelType and ShippingSerialID must be set.');
-        $pack->exportToXml($xml);
+        $this->assertSame('00000000001000000014', (string)$xml->Pack->ShippingSerialID);
+        $this->assertSame('7123456', (string)$xml->Pack->CarrierPackageID);
     }
 
     public function testExportToXmlInvalidQualifier(): void
     {
-        $pack = new Pack('X', 'a');
+        $pack = new Pack('X', '0', '0');
         $xml = new SimpleXMLElement('<root/>');
         $this->expectException(ElementInvalid::class);
         $this->expectExceptionMessage('Pack: Invalid PackLevelType.');
         $pack->exportToXml($xml);
+    }
+
+    /**
+     * @link https://www.barcode.graphics/check-digit-calculator/
+     */
+    public function testGenerateSSCC(): void
+    {
+        $this->assertSame('00000000001000000014', Pack::generateSSCC('00000001', '00000001'));
+        $this->assertSame('00100000001000000011', Pack::generateSSCC('00000001', '00000001', '1'));
+        $this->assertSame('00000000007734927436', Pack::generateSSCC('00000007', '73492743'));
+        $this->assertSame('00100000007734927433', Pack::generateSSCC('00000007', '73492743', '1'));
+        $this->assertSame('00005789000002318276', Pack::generateSSCC('05789000', '00231827'));
+        $this->assertSame('00005789000002318276', Pack::generateSSCC('05789000', '231827'));
+    }
+
+    public function testGenerateSSCCInvalid(): void
+    {
+        $this->expectExceptionMessage(
+            'Invalid GS1 Company Prefix or Serial Reference - combined, they must not exceed 16 characters.'
+        );
+        Pack::generateSSCC('000000001', '000000001');
     }
 }
